e3631e56b823a62e444a019b595998376b4ea64d
gunaybabayeva21 <gunaybabayeva032@gmail.com>
2023-02-28 11:34:56 +0400

    new project

---
+++ dovrler/Program.cs
@@ -0,0 +1,39 @@
+﻿//Task4
+
+//int num= 345;
+//int x =3;
+//int y =4;
+//int z =3;
+
+//for (int i = 0; i < 345; i++)
+//{
+//    if (x > z)
+//    {
+//        Console.WriteLine("x en boyukdur");
+//    }
+
+
+
+//    else
+//    {
+//        Console.WriteLine("z boyukdur");
+
+//    }
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
